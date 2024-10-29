9441942c489efde042988d4d6698083178ac5bcb
Nick Kalas <nick.kalas@proton.me>
2024-10-29 16:33:33 +0200

    Initial commit

--- src/classes.js
+++ src/classes.js
@@ -1,4 +1,4 @@
-export class Node {
+class Node {
     constructor(data) {
         this.data = data;
         this.left = null;
@@ -6,6 +6,43 @@ export class Node {
     }
 }
 
-class Tree {
-    constructor(arr) {}
+export class Tree {
+    constructor(arr) {
+        this.arr = arr;
+        this.root = this.buildTree();
+    }
+
+    // turns array into a balanced binary tree full of Node objects
+    buildTree() {
+        //return the level-0 root node
+    }
+
+    insert(value) {}
+    deleteItem(value) {}
+
+    find(value) {
+        //returns the node with the given value
+    }
+
+    levelOrder(callback) {
+        //traverse the tree in breadth-first level order and call the callback on each node
+    }
+
+    inOrder(callback) {}
+    preOrder(callback) {}
+    postOrder(callback) {}
+
+    height(node) {
+        //returns the given node’s height
+    }
+
+    depth(node) {
+        //returns the given node’s depth
+    }
+
+    isBalanced() {
+        //checks if the tree is balanced
+    }
+
+    rebalance() {}
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import "./normalize.css";
 import "./styles.css";
-import { Node } from "./classes";
+import { Node, Tree } from "./classes";
 
-// turns array into a balanced binary tree full of Node objects
-function buildTree(array) {
-        
-    
-}
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
 document.addEventListener("DOMContentLoaded", () => {
-    buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+    tree.buildTree();
 });
-
